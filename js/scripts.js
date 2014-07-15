99b7ced2c1158913089bd7118297643ef7deb13d
Diane Douglas <douglasdiane0@gmail.com>
2014-07-15 16:30:48 -0700

    Adds second spec and rewords the it statement for the first spec for clarity. First test makes sure that a shift amount of 0 outputs the same phrase, and it only deals with a one-letter phrase. The second test also uses only a one-letter phrase but applies a shift amount of 1. Made both tests pass by implementing an array of alphabetical characters.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 var shiftCipher = function(shiftAmount, inputPhrase){
-  return "a";
+  var currentChar = inputPhrase; //deal with parsing into individual characters later here.
+
+  var alphabetChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var currentCharIndex = alphabetChars.indexOf(currentChar);
+  var encodedChar = alphabetChars[currentCharIndex + shiftAmount];
+
+  return encodedChar;
 }
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,8 @@
 describe("shiftCipher", function(){
-  it("Takes a phrase and a shift amount of 0 and outputs the same phrase.", function(){
+  it("Takes a one-letter phrase and a shift amount of 0 and outputs the same phrase.", function(){
     shiftCipher(0, "a").should.equal("a");
+  });
+  it("Takes a one-letter phrase and a shift amount of 1 and outputs the encoded one-letter phrase.", function(){
+    shiftCipher(1, "a").should.equal("b");
   });
 });
