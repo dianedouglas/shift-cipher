ef4038ef471893d2f3d1d4a607ff39ea3b2f82a3
dianedouglas <diane.douglas1@gmail.com>
2014-07-16 21:38:44 -0700

    Refactoring: took out extra uses of looking up the current character in the input phrase. Instead this happens once and the value is stored in the variable currentChar, which is reused. Also cleaned up comments, and declared currentCharIndex inside of the for loop instead of at the top of the file since it isn't reused. All tests still pass.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 var shiftCipher = function(shiftAmount, inputPhrase){
   var alphabetChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var numericalChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var currentChar; //inputPhrase is split into individual characters which are put into currentChar
-  var currentCharIndex; //index of where the current character lies in the alphabetChars array.
+  var currentChar; //step through input phrase one character at a time, storing current character here.
   var encodedChar;  //after the shift has been applied to currentChar, encodedChar is the output.
   var encodedPhrase = ""; //all encodedChars will be stored here and this will be the final encoded phrase to be returned.
 
-  for (var i = 0; i < inputPhrase.length; i++){ //step through input phrase one character at a time.
-    if(inputPhrase.charAt(i) === " "){
+  for (var i = 0; i < inputPhrase.length; i++){ 
+      currentChar = inputPhrase.charAt(i); 
+    if(currentChar === " "){ //if current character is a space, store it as the encoded character without changing it.
       encodedChar = " ";
-    } else if(numericalChars.indexOf(inputPhrase.charAt(i)) >= 0){
-      currentChar = inputPhrase.charAt(i);  //store the current character
-      currentCharIndex = numericalChars.indexOf(currentChar); //store its index from numericalChars
-      encodedChar = numericalChars[currentCharIndex + shiftAmount]; //encode the char.
-    } else {
-      currentChar = inputPhrase.charAt(i);  //store the current character
-      currentCharIndex = alphabetChars.indexOf(currentChar); //store its index from alphabetChars
-      encodedChar = alphabetChars[currentCharIndex + shiftAmount]; //encode the char.
+    } else if(numericalChars.indexOf(currentChar) >= 0){  //if it's a number
+      var currentCharIndex = numericalChars.indexOf(currentChar); //store its index from numericalChars array
+      encodedChar = numericalChars[currentCharIndex + shiftAmount]; //get the encoded char by applying shift amount to array.
+    } else if(alphabetChars.indexOf(currentChar) >=0){  //if it's a letter, do the same with alphabetChars array.
+      var currentCharIndex = alphabetChars.indexOf(currentChar); 
+      encodedChar = alphabetChars[currentCharIndex + shiftAmount]; 
     }
     encodedPhrase += encodedChar; //add it to the encoded phrase.
   }
